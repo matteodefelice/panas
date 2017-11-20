a0612b493938c7dfa758b7002137b064f1a0fd76
Matteo De Felice <matteo.defelice@gmail.com>
2017-11-20 10:48:35 +0100

    Update choropleth
    Added NUTS shapefile, improve doc and managed both UK and GR codes.

--- R/get_european_choropleth.R
+++ R/get_european_choropleth.R
@@ -1,14 +1,14 @@
 #' Prepare a ggplot2-based choropleth on European national (NUTS0) countries.
 #' @param input_data A data frame containing the data to be mapped. It should contain two columns: the first containing the ISO ALPHA-2
-#' code of the country, the second the value that should be mapped
+#' code of the country, the second the value that should be mapped.
 #' @return A ggplot2 object
+#' @section Details
+#' The country codes should be a NUTS code, a two-letter almost identical to ISO 3166-1 alpha-2 code. The only exceptions are \code{UK} instead of \code{GB} and \code{EL} instead of \code{GR}. 
 #' @export
 #'
 get_european_choropleth <- function(input_data) {
-  library(dplyr)
-  library(ggplot2)
   #### Load shapefiles
-  load('~/Documents/Work/ecemathon/chorofunction/eumap_adm0.rda')
+  load(system.file("NUTS", "eumap_adm0.rda", package = "panas"))
   #### Check the input data frame
   if (!is.data.frame(input_data)) {
     stop('The input_data is not a data frame')
@@ -18,15 +18,15 @@ get_european_choropleth <- function(input_data) {
   #### Homogenise the column names
   colnames(input_data) = c('area','value')
   ## Match EUMAP ADM0 names
-  input_data = mutate(input_data, area = if_else(area == 'GR', 'EL', area))
+  input_data = mutate(input_data, area = if_else(area == 'GR', 'EL', area)) %>%
+    mutate(input_data, area = if_else(area == 'GB', 'UK', area))
+  
   jj = left_join(eumap_adm0, input_data, by = c('id' = 'area'))
 
   g = ggplot(jj, aes(x = long, y = lat, group = group,
                      fill = value)) +
     geom_polygon() +
     geom_path(color = 'gray50', size = 0.25) +
-    # scale_fill_brewer(palette = "PiYG", na.value = 'lightgrey', name = 'Corr.', drop = F) +
-
     coord_quickmap(ylim = c(35, 65), xlim = c(-15, 25)) +
     xlab('Longitude') + ylab('Latitude')
   return(g)
